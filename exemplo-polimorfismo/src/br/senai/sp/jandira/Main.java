78c3671b8756c9c20275b409f7e95b9eaa550cd6
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2023-10-18 15:54:49 -0300

    exemplo polimorfismo

---
+++ exemplo-polimorfismo/src/br/senai/sp/jandira/Main.java
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira;
+
+import br.senai.sp.jandira.model.Animal;
+import br.senai.sp.jandira.model.Vaca;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal vaca = new Vaca();
+        vaca.fazerSom();
+
+    }
+}
---
+++ exemplo-polimorfismo/src/br/senai/sp/jandira/model/Vaca.java
@@ -0,0 +1,10 @@
+package br.senai.sp.jandira.model;
+
+public class Vaca extends Animal {
+
+    @Override
+    public void fazerSom(){
+        System.out.println("Muuuuuuuu");
+    }
+
+}
